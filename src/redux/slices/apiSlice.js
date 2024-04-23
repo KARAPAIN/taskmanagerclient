c3d6f4b2f11810ce1279097e526ea3b88085e0b5
KARAPAIN <88405360+KARAPAIN@users.noreply.github.com>
2024-04-23 23:29:25 +0100

    Update apiSlice.js

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URI = import.meta.env.VITE_APP_BASE_URL;
-const API_URI = "https://gestiondesprojects-f1710f0d8ad9.herokuapp.com";
+const API_URI = "https://gestiondesprojectsserver.onrender.com";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI + "/api" });
 
